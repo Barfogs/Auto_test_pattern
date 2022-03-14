9fdb233dc4810ffd75a175f43c53237f26580c65
David <davidqacase@gmail.com>
2022-03-14 21:19:28 +0300

    Тестирование корзины

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -4,9 +4,9 @@ from .locators import BasketPageLocators
 
 class BasketPage(BasePage):
     def empty_cart_null(self):
-        assert self.is_element_present(*BasketPageLocators.CONTENT_INNER), "Имеется уведомление о пустой корзине"
+        assert self.is_element_present(*BasketPageLocators.CONTENT_INNER), "Имеется hint 'Пустая корзина'"
         assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS), "Козина не имеет товаров"
 
     def empty_cart_full(self):
-        assert self.is_not_element_present(*BasketPageLocators.CONTENT_INNER), "Уведомление о пустой корзине отсутствует"
+        assert self.is_not_element_present(*BasketPageLocators.CONTENT_INNER), "Hint 'Пустая корзина' отсутствует"
         assert self.is_element_present(*BasketPageLocators.BASKET_ITEMS), "Козина имеет товары"
