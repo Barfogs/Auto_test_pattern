2f15c1e7db9cc43dfaedae7509c4e58ad5a05025
Barfogs <88886083+Barfogs@users.noreply.github.com>
2022-03-15 00:36:01 +0300

    Delete test_login_page.py

--- test_login_page.py
+++
@@ -1,8 +0,0 @@
-from .pages.login_page import LoginPage
-
-
-def test_guest_go_to_login_page(browser):
-    link = "https://selenium1py.pythonanywhere.com/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_page()
